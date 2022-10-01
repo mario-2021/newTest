0b0c823ac67d733938c220089f8dbc7ae98d9475
mario-2021 <81588722+mario-2021@users.noreply.github.com>
2022-10-02 01:54:02 +0200

    Add files via upload

---
+++ JS/MigFarm.js
@@ -0,0 +1,10 @@
+$(function () {
+    'use strict';
+    $('.toggle-sidebar').on('click', function () {
+        $('.content-area, .sidebar').toggleClass('no-sidebar');
+    });
+    $('.toggle-submenu').on('click', function () {
+        $(this).find('.fa-angle-right').toggleClass('down');
+        $(this).next('.child-links').slideToggle();
+    });
+});
---
+++ JS/dist/newDesign.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(function () {
+  'use strict';
+
+  $('.toggle-sidebar').on('click', function () {
+    $('.content-area, .sidebar').toggleClass('no-sidebar');
+  });
+  $('.toggle-submenu').on('click', function () {
+    $(this).find('.fa-angle-right').toggleClass('down');
+    $(this).next('.child-links').slideToggle();
+  });
+});
